119fab5d561ba6e77a5262be896e2eebcf01f351
Noor-Ul-Ain16 <97411191+Noor-Ul-Ain16@users.noreply.github.com>
2026-08-28 00:16:05 +0500

    Edit Comment

--- app/be-part-of-it/sponsorship-form/page.tsx
+++ app/be-part-of-it/sponsorship-form/page.tsx
@@ -78,7 +78,7 @@ export default function SponsorshipPage() {
     }
   };
 
-  // Animation configurations
+  //Animation configurations
   const fadeInUp = {
     hidden: { opacity: 0, y: 20 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
